08838f090992a6be25686677f1193ff77811501e
MariaDuda <mariabevenuto062@gmail.com>
2022-04-05 13:39:59 -0300

    adicionando o axios

--- ceep/src/pages/Cadastro/index.js
+++ ceep/src/pages/Cadastro/index.js
@@ -1,5 +1,6 @@
 import Input from "../../componentes/Inputs"
 import { useState } from "react"
+import axios from "axios";
 import { Inputs, Botao, Legenda } from "./style.js";
 import { Link } from "react-router-dom";
 import Apertar from "../../componentes/Botao";
